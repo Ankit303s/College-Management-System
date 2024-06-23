1d40e0c084f37d0d6eb976034efa7d57ce99a387
Ankit <ankitsinghh04@gmail.com>
2024-06-23 09:53:19 +0530

    image slider fixed

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const slider = document.querySelector('.slider');
 const sl_images = document.querySelectorAll('.image');
 sl_length = sl_images.length
 let slidenumber = 1
+
 //for right arrow
 const next_slide = () => {
     slider.style.transform = `translateX(-${slidenumber*100}vw)`
     slidenumber++
-}
+};
+
 const first_slide = () => {
     slider.style.transform = 'translateX(0px)'
     slidenumber = 1;
-}
+};
 
 right_slider.addEventListener( 'click', () => {
     slidenumber < sl_length ? next_slide() : first_slide()
@@ -25,10 +27,9 @@ const previous_slide = () => {
 }
 
 const last_slide = () => {
-    slider.style.transform = `translateX(-${(sl_length-1)*100}vh)`
-    slidenumber = slidenumber.length;
+    slider.style.transform = `translateX(-${(sl_length-1)*100}vw)`
+    slidenumber = sl_length;
 }
 left_slider.addEventListener( 'click', () => {
-    previous_slide()
     slidenumber > 1 ? previous_slide() : last_slide()
 });
