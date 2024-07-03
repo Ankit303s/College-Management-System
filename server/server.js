a092bb4bd250258881bcdf563b9f4386af645ae2
Ankit <ankitsinghh04@gmail.com>
2024-07-03 10:13:55 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'client')));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Import Routes
 const authRoute = require('./routes/auth');
